6987e5ed486b9624f644c0d2bcc5baa937cd4b52
Nils Ceberg <nils.ceberg@gmail.com>
2015-08-20 09:24:51 +0200

    Fixa input :)

--- tetris/game.py
+++ tetris/game.py
@@ -3,18 +3,28 @@ import time
 
 class Game:
     def __init__(self):
-        pass
+        self.tick_rate = 1
 
     def __enter__(self):
         self.gui = tetris.gui.GUI()
         return self
 
     def run(self):
+        self.start = time.time()
+        self.last_tick = self.start
+
         while True:
             self.gui.update_game(None)
+            
+            action = self.gui.get_input((self.last_tick + self.tick_rate) - time.time())
+            if action != None:
+                self.gui.status_window.addch(action)
 
-            while len(self.gui.input_queue > 0):
-                pass
+            self.gui.game_window.addstr(1, 1, str(time.time()) + " " + str(self.last_tick))
+
+            if time.time() > (self.last_tick + self.tick_rate):
+                self.gui.status_window.addch('T')
+                self.last_tick = time.time()
 
 
     def __exit__(self, exc_type, exc_value, traceback):
--- tetris/gui.py
+++ tetris/gui.py
@@ -5,6 +5,11 @@ class GUI:
         self.stdscr = None
         self.input_queue = []
 
+        self.key_binds = {
+                    curses.KEY_UP: 'A',
+                    curses.KEY_DOWN: 'B'
+                }
+
         self.setup_screen()
         self.setup_windows()
 
@@ -13,7 +18,6 @@ class GUI:
         curses.nocbreak()
         self.stdscr.keypad(0)
         curses.curs_set(1)
-        curses.nodelay(1)
         curses.echo()
         curses.endwin()
 
@@ -29,17 +33,31 @@ class GUI:
         self.game_window = curses.newwin(30, 40, 0, 0)
         self.status_window = curses.newwin(30, 20, 0, 40)
 
+        self.input_window = curses.newwin(0, 0, 0, 60) 
+        self.input_window.keypad(1)
+
         self.game_window.box()
         self.status_window.box()
+
+        self.game_window.addstr(1, 1, "hello")
 
         self.game_window.refresh()
         self.status_window.refresh()
 
     def update_game(self, world):
-        input = self.stdscr.getch()
+        self.status_window.box()
+        self.status_window.refresh()
+        self.game_window.box()
+        self.game_window.refresh()
 
-        if input == curses.KEY_LEFT:
-            self.input_queue.append("LEFT")
+    def get_input(self, timeout):
+        self.input_window.timeout(int(timeout * 1000))
+        key = self.input_window.getch()
+        if key in self.key_binds:
+            return self.key_binds[key]
+        else:
+            return None
+        
 
     def update_status(self, next_block, score):
         pass
