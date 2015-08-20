3f29b0440aba6e63e30a5761893b89024fea4905
Joakim Junttila <joajun@student.chalmers.se>
2015-08-20 09:54:06 +0200

    Lagt till poäng och sänker tick rate beroende på poäng

--- tetris/game.py
+++ tetris/game.py
@@ -4,6 +4,8 @@ import time
 class Game:
     def __init__(self):
         self.tick_rate = 1
+        self.points = 0
+
 
     def __enter__(self):
         self.gui = tetris.gui.GUI()
@@ -17,6 +19,7 @@ class Game:
             action = self.gui.get_input((self.last_tick + self.tick_rate) - time.time())
             if action != None:
                 self.gui.status_window.addch(action)
+
                 self.gui.draw_game(None)
 
             self.gui.game_window.addstr(1, 1, str(time.time()) + " " + str(self.last_tick))
@@ -24,6 +27,10 @@ class Game:
             if time.time() > (self.last_tick + self.tick_rate):
                 self.gui.status_window.addch('T')
                 self.last_tick = time.time()
+
+                self.points += 1
+                self.tick_rate = 0.95 ^ self.points
+
 
                 self.gui.draw_game(None)
                 self.gui.draw_status(None, None)
