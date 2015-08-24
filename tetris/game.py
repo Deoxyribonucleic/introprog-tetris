8e9b46beb79885d34d43e1fc4317dac6b257e5c1
Nils Ceberg <nils.ceberg@gmail.com>
2015-08-24 13:50:11 +0200

    Refactor block definitions

--- tetris/game.py
+++ tetris/game.py
@@ -1,15 +1,20 @@
 import tetris.gui
+import tetris.blocks
+
 import time
 
 class Game:
     def __init__(self):
         self.tick_rate = 1
         self.points = 0
+
         self.world_width = 13
         self.world_height = 15
         
         self.world = World(self.world_width, self.world_height)
-        self.world.add_block(Block([['X', None, None], ['X', 'X', 'X']]))
+
+        self.world.add_block(Block(tetris.blocks.blocks[0]))
+        self.world.add_block(Block(tetris.blocks.blocks[1], 3, 5))
 
 
     def __enter__(self):
@@ -53,10 +58,12 @@ class World:
     def add_block(self, block):
         for (ypos, yline) in enumerate(block.shape):
             for (xpos, xline) in enumerate(yline):
-                self.world[block.ypos+ypos][block.xpos+xpos] = block.shape[ypos][xpos]
+                self.world[block.ypos+ypos][block.xpos+xpos] = block.representation if block.shape[ypos][xpos] == 1 else None
 
 class Block:
-    def __init__(self, shape, xpos=0, ypos=0):
-        self.shape = shape
+    def __init__(self, block_type, xpos=0, ypos=0):
+        self.shape = block_type["shape"]
+        self.representation = block_type["representation"]
         self.xpos = xpos
         self.ypos = ypos
+
