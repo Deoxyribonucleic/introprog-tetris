e19ee380a947b27b781c35f96f13ca26b4ace80c
Nils Ceberg <nils.ceberg@gmail.com>
2015-08-24 14:12:01 +0200

    Draw current block

--- tetris/game.py
+++ tetris/game.py
@@ -14,13 +14,18 @@ class Game:
         
         self.world = tetris.world.World(self.world_width, self.world_height)
 
-        self.world.add_block(tetris.block.Block(tetris.block.blocks[0]))
-        self.world.add_block(tetris.block.Block(tetris.block.blocks[1], 3, 5))
+        self.world.add_block(tetris.block.Block(tetris.block.blocks[0], 0, self.world_height - 5))
+        self.world.add_block(tetris.block.Block(tetris.block.blocks[1], 2, self.world_height - 4))
 
+        self.next_block = self.create_random_block()
+        self.current_block = None
 
     def __enter__(self):
         self.gui = tetris.gui.GUI()
         return self
+
+    def create_random_block(self):
+        return tetris.block.Block(tetris.block.blocks[1], 3, 3)
 
     def run(self):
         self.start = time.time()
@@ -28,13 +33,17 @@ class Game:
 
         # render once at start
         self.gui.draw_status(None, None)
-        self.gui.draw_game(self.world)
+        self.gui.draw_game(self.world, self.current_block)
 
         while True:
+            if self.current_block == None:
+                self.current_block = self.next_block
+                self.next_block = self.create_random_block()
+
             action = self.gui.get_input((self.last_tick + self.tick_rate) - time.time())
             if action != None:
                 self.gui.status_window.addch(action)
-                self.gui.draw_game(self.world)
+                self.gui.draw_game(self.world, self.current_block)
 
             if time.time() > (self.last_tick + self.tick_rate):
                 self.tick()
@@ -46,7 +55,9 @@ class Game:
         self.points += 1
         self.tick_rate = 0.95 ** self.points
 
-        self.gui.draw_game(self.world)
+        self.current_block.ypos+=1
+
+        self.gui.draw_game(self.world, self.current_block)
         self.gui.draw_status(None, None)
 
     def __exit__(self, exc_type, exc_value, traceback):
--- tetris/worldrenderer.py
+++ tetris/worldrenderer.py
@@ -8,9 +8,15 @@ class WorldRenderer:
         for y in range(len(world.world)):
             for x in range(len(world.world[y])):
                 if world.world[y][x]:
-                    self._draw_block(x, y, world.world[y][x]) 
+                    self._draw_block_element(x, y, world.world[y][x]) 
 
-    def _draw_block(self, x, y, block_type):
+    def draw_block(self, block):
+        for y in range(len(block.shape)):
+            for x in range(len(block.shape[y])):
+                if block.shape[y][x]:
+                    self._draw_block_element(x + block.xpos, y + block.ypos, block.representation if block.shape[y][x] == 1 else None) 
+
+    def _draw_block_element(self, x, y, block_type):
         for offset_x in range(0, 3):
             for offset_y in range(0, 2):
                 self.window.addch(y * 2 + offset_y + 1, x * 3 + offset_x + 1, block_type) # +1 on both to accomodate for border
