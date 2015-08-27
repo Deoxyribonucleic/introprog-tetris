ba6b2f3b3d028dbb6e4e697f9b2e5674ee42fc68
Martin Löfgren <martin.c.lofgren@gmail.com>
2015-08-27 14:14:35 +0200

    Added height option

--- main.py
+++ main.py
@@ -2,6 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import tetris.game
+import argparse
 
-with tetris.game.Game() as game:
+parser = argparse.ArgumentParser(description='Play some Textris')
+parser.add_argument('--height',
+                    dest='height',
+                    type=int,
+                    help='Specify the height of the window between 12 and 22. Defaults to 22.')
+args = parser.parse_args()
+
+if args.height < 12 or args.height > 22:
+    print('Height must be between 12 and 22')
+    exit()
+
+with tetris.game.Game(args.height) as game:
     game.run()
